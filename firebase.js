2a67a0b0437ce530ba33e56d3fab42267956795f
magassahamed2024-debug <magassahamed2024@gmail.com>
2026-07-10 03:50:12 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,39 +1,34 @@
-// Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
-getFirestore,
-collection,
-addDoc
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-apiKey: "TON_API_KEY",
-
-authDomain: "TON_PROJET.firebaseapp.com",
-
-projectId: "TON_PROJET",
-
-storageBucket: "TON_PROJET.appspot.com",
-
-messagingSenderId: "000000000",
-
-appId: "TON_APP_ID"
-
+  apiKey: "AIzaSyDjlKsXycr-steaZEkMLRdxrpOS8aUSQ8c",
+  authDomain: "rencontre-inattendue.firebaseapp.com",
+  projectId: "rencontre-inattendue",
+  storageBucket: "rencontre-inattendue.firebasestorage.app",
+  messagingSenderId: "48557100293",
+  appId: "1:48557100293:web:feed73a34545c19d9a7b6c"
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-export async function envoyerReponses(reponses){
-
-await addDoc(collection(db,"reponses"),{
-
-date:new Date(),
-
-...reponses
-
-});
-
+export async function enregistrerReponse(data) {
+  await addDoc(collection(db, "reponses"), data);
 }
+
+export async function lireReponses() {
+  const snapshot = await getDocs(collection(db, "reponses"));
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
+}
+
+export { db };
